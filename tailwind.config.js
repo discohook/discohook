d8f1c3a17bbb05334f7d978db3bda15892775f3d
shay <43248357+shayypy@users.noreply.github.com>
2023-09-15 21:29:31 -0500

    chore: helvetica fallback

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: ["Whitney", "sans-serif"],
+      sans: ["Whitney", "Helvetica", "sans-serif"],
     },
     extend: {
       colors: {
