0f8ab9ee096ae2359f460f612f1a058cb0d5ae56
shay <43248357+shayypy@users.noreply.github.com>
2023-09-16 14:14:32 -0500

    update: content weight

--- app/components/preview/Message.tsx
+++ app/components/preview/Message.tsx
@@ -30,17 +30,19 @@ export const Message: React.FC<{
         />
       </div>
       <div className="grow">
-        <p className="font-medium leading-none h-4">
-          <span className="hover:underline cursor-pointer underline-offset-1 decoration-1">{username}</span>
+        <p className="leading-none h-4">
+          <span className="hover:underline cursor-pointer underline-offset-1 decoration-1 font-semibold">{username}</span>
           {badge && (
-            <span className="ml-1 mt-[0.75px] text-[10px] rounded px-1.5 py-px bg-blurple text-white items-center inline-flex h-4">{badge}</span>
+            <span className="font-medium ml-1 mt-[0.75px] text-[10px] rounded px-1.5 py-px bg-blurple text-white items-center inline-flex h-4">{badge}</span>
           )}
-          <span className="ml-1 cursor-default text-xs align-baseline text-[#5C5E66] dark:text-[#949BA4]">
+          <span className="font-medium ml-1 cursor-default text-xs align-baseline text-[#5C5E66] dark:text-[#949BA4]">
             Today at {(date ?? new Date()).toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' })}
           </span>
         </p>
         {message.content && (
-          <Markdown text={message.content} features="all" resolved={resolved} />
+          <div className="font-medium text-base">
+            <Markdown text={message.content} features="all" resolved={resolved} />
+          </div>
         )}
         {message.embeds && message.embeds.length > 0 && (
           <div className="space-y-1 mt-1">
