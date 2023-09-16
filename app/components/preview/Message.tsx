506f51e30ca04e129ed1fb862ff47407135ede3b
shay <43248357+shayypy@users.noreply.github.com>
2023-09-15 21:28:59 -0500

    feat: content markdown

--- app/components/preview/Message.tsx
+++ app/components/preview/Message.tsx
@@ -1,19 +1,21 @@
 import { APIWebhook } from "discord-api-types/v10";
 import { QueryData } from "~/types/QueryData";
 import { cdn } from "~/util/discord";
+import { Markdown } from "./Markdown";
 
 export const Message: React.FC<{
   message: QueryData["messages"][number]["data"];
   webhook?: APIWebhook;
   compact?: boolean;
   theme?: "light" | "dark";
-}> = ({ message, webhook }) => {
+  date?: Date;
+}> = ({ message, webhook, date }) => {
   const username = message.author?.name ?? webhook?.name ?? "Boogiehook",
     avatarUrl =
       message.author?.icon_url ??
       (webhook?.avatar
         ? cdn.avatar(webhook.id, webhook.avatar, { size: 64 })
-        : cdn.defaultAvatar(1)),
+        : cdn.defaultAvatar(5)),
     badge: string | undefined = "BOT";
 
   return (
@@ -31,7 +33,13 @@ export const Message: React.FC<{
           {badge && (
             <span className="ml-1 mt-[0.75px] text-[10px] rounded px-1.5 py-px bg-blurple text-white items-center inline-flex h-4">{badge}</span>
           )}
+          <span className="ml-1 cursor-default text-xs align-baseline text-[#5C5E66] dark:text-[#949BA4]">
+            Today at {(date ?? new Date()).toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' })}
+          </span>
         </p>
+        {message.content && (
+          <Markdown text={message.content} features="all" />
+        )}
       </div>
     </div>
   );
