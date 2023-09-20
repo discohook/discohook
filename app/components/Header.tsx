90a472aa550b95e1fb0982b653f8165f6336fd7b
shay <43248357+shayypy@users.noreply.github.com>
2023-09-20 17:38:55 -0500

    feat: embed example

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,22 +1,32 @@
 import { useSearchParams } from "@remix-run/react";
 import { ButtonStyle } from "discord-api-types/v10";
 import { useState } from "react";
+import { ExampleModal } from "~/modals/ExampleModal";
 import { HelpModal } from "~/modals/HelpModal";
 import { Button } from "./Button";
 
 export const Header: React.FC<{}> = ({}) => {
   const [searchParams] = useSearchParams();
-  const [open, setOpen] = useState(searchParams.get("m") === "help");
+  const [helpOpen, setHelpOpen] = useState(searchParams.get("m") === "help");
+  const [exampleOpen, setExampleOpen] = useState(searchParams.get("m") === "embed-example");
 
   return (
     <div className="sticky top-0 left-0 z-10 bg-slate-50 shadow-md w-full px-4 h-12 flex">
-      <HelpModal open={open} setOpen={setOpen} />
+      <HelpModal open={helpOpen} setOpen={setHelpOpen} />
+      <ExampleModal open={exampleOpen} setOpen={setExampleOpen} />
       <Button
         className="my-auto ml-auto"
         discordstyle={ButtonStyle.Secondary}
-        onClick={() => setOpen(true)}
+        onClick={() => setHelpOpen(true)}
       >
         Help
+      </Button>
+      <Button
+        className="my-auto ml-2"
+        discordstyle={ButtonStyle.Secondary}
+        onClick={() => setExampleOpen(true)}
+      >
+        Embed Example
       </Button>
     </div>
   );
---
+++ app/modals/ExampleModal.tsx
@@ -0,0 +1,66 @@
+import { Button } from "~/components/Button";
+import { Message } from "~/components/preview/Message";
+import { Modal, ModalProps } from "./Modal";
+
+export const ExampleModal = (props: ModalProps) => {
+  return (
+    <Modal title="Embed Example" {...props}>
+      <p>
+        Discord messages come in all shapes and sizes. Here's an example message that
+        showcases every text field.
+      </p>
+      <div className="mt-4">
+        <Message
+          message={{
+            content: "Content",
+            embeds: [
+              {
+                author: {
+                  name: "Author",
+                },
+                title: "Title",
+                description: "Description\n\n<-- Sidebar color",
+                color: 0xeb459f,
+                fields: [
+                  {
+                    name: "Field 1 (inline)",
+                    value: "Field 1 value",
+                    inline: true,
+                  },
+                  {
+                    name: "Field 2 (inline)",
+                    value: "Field 2 value",
+                    inline: true,
+                  },
+                  {
+                    name: "Field 3 (inline)",
+                    value: "Field 3 value",
+                    inline: true,
+                  },
+                  {
+                    name: "Field 4 (not inline)",
+                    value: "Field 4 value",
+                    inline: false,
+                  },
+                  {
+                    name: "Field 5 (not inline)",
+                    value: "Field 5 value",
+                    inline: false,
+                  },
+                ],
+                footer: {
+                  text: "Footer",
+                },
+              },
+            ],
+          }}
+        />
+      </div>
+      <div className="flex w-full mt-4">
+        <Button onClick={() => props.setOpen(false)} className="mx-auto">
+          OK
+        </Button>
+      </div>
+    </Modal>
+  );
+};
