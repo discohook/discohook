f8585c1acf6bb87de547f349059d922904f8107c
shay <43248357+shayypy@users.noreply.github.com>
2023-09-20 14:01:11 -0500

    fix: inline field placement

--- app/components/preview/Embed.tsx
+++ app/components/preview/Embed.tsx
@@ -1,4 +1,4 @@
-import { APIEmbed, APIEmbedImage } from "discord-api-types/v10";
+import { APIEmbed, APIEmbedField, APIEmbedImage } from "discord-api-types/v10";
 import { PartialResource } from "~/types/Resources";
 import { Markdown } from "./Markdown";
 
@@ -7,6 +7,25 @@ export const Embed: React.FC<{
   extraImages?: APIEmbedImage[];
   resolved?: Record<string, PartialResource>;
 }> = ({ embed, resolved }) => {
+  const fieldLines: APIEmbedField[][] = [];
+  for (const field of embed.fields ?? []) {
+    const currentLine = fieldLines[fieldLines.length - 1];
+    if (!currentLine) {
+      fieldLines.push([field]);
+    } else {
+      const lastField = currentLine[currentLine.length - 1];
+      if (!lastField) {
+        // This shouldn't happen
+        continue;
+      }
+      if (field.inline && lastField.inline && currentLine.length < 3) {
+        currentLine.push(field);
+      } else {
+        fieldLines.push([field]);
+      }
+    }
+  }
+
   return (
     <div>
       <div
@@ -75,56 +94,61 @@ export const Embed: React.FC<{
             />
           </div>
         )}
-        {embed.fields && embed.fields.length > 0 && (
+        {fieldLines.length > 0 && (
           <div className="text-sm leading-[1.125rem] grid col-start-1 col-end-2 gap-2 mt-2 min-w-0">
-            {embed.fields.map((field, i) => {
-              let inlineBound = [1, 13];
-              if (field.inline) {
-                const fields = embed.fields!;
-                const beforeLast = fields[i - 2],
-                  last = fields[i - 1],
-                  next = fields[i + 1],
-                  afterNext = fields[i + 2];
+            {fieldLines.map((line, i) => (
+              <div
+                key={`message-preview-embed-fields-row-${i}`}
+                className="contents"
+                data-field-row-index={i}
+              >
+                {line.map((field, colIndex) => {
+                  let inlineBound = [1, 13];
+                  if (field.inline) {
+                    if (line.length === 3) {
+                      if (colIndex === 2) {
+                        inlineBound = [9, 13];
+                      } else if (colIndex === 1) {
+                        inlineBound = [5, 9];
+                      } else {
+                        inlineBound = [1, 5];
+                      }
+                    } else if (line.length === 2) {
+                      if (colIndex === 1) {
+                        inlineBound = [7, 13];
+                      } else {
+                        inlineBound = [1, 7];
+                      }
+                    }
+                  }
 
-                if (last?.inline && beforeLast?.inline) {
-                  inlineBound = [9, 13];
-                } else if (last?.inline && !next?.inline) {
-                  inlineBound = [7, 13];
-                } else if (next?.inline && afterNext?.inline) {
-                  inlineBound = [1, 5];
-                } else if (last?.inline && next?.inline) {
-                  inlineBound = [5, 9];
-                } else if (next?.inline && !afterNext?.inline) {
-                  inlineBound = [1, 7];
-                }
-              }
-
-              return (
-                <div
-                  key={`message-preview-embed-field-${i}`}
-                  className="min-w-0"
-                  data-field-index={i}
-                  style={{
-                    gridColumn: `${inlineBound[0]} / ${inlineBound[1]}`,
-                  }}
-                >
-                  <div className="font-semibold mb-px">
-                    <Markdown
-                      text={field.name}
-                      features={["basic", "emojis"]}
-                      resolved={resolved}
-                    />
-                  </div>
-                  <div>
-                    <Markdown
-                      text={field.value}
-                      features="all"
-                      resolved={resolved}
-                    />
-                  </div>
-                </div>
-              );
-            })}
+                  return (
+                    <div
+                      key={`message-preview-embed-fields-row-${i}-field-${colIndex}`}
+                      data-field-subrow-index={i}
+                      style={{
+                        gridColumn: `${inlineBound[0]} / ${inlineBound[1]}`,
+                      }}
+                    >
+                      <div className="font-semibold mb-px">
+                        <Markdown
+                          text={field.name}
+                          features={["basic", "emojis"]}
+                          resolved={resolved}
+                        />
+                      </div>
+                      <div>
+                        <Markdown
+                          text={field.value}
+                          features="all"
+                          resolved={resolved}
+                        />
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            ))}
           </div>
         )}
       </div>
