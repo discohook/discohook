d2c7343998cc080bf2dff7afab6bab4ed39a56ac
shay <43248357+shayypy@users.noreply.github.com>
2023-09-23 19:25:37 -0500

    feat: move, copy, delete messages

--- app/components/editor/MessageEditor.tsx
+++ app/components/editor/MessageEditor.tsx
@@ -1,7 +1,16 @@
 import { QueryData } from "~/types/QueryData";
 import { Button } from "../Button";
+import { CoolIcon } from "../CoolIcon";
 import { TextArea } from "../TextArea";
-import { EmbedEditor } from "./EmbedEditor";
+import { EmbedEditor, getEmbedText } from "./EmbedEditor";
+
+export const getMessageText = (
+  message: QueryData["messages"][number]["data"]
+): string | undefined =>
+  message.content ??
+  (message.embeds
+    ? message.embeds.map(getEmbedText).find((t) => !!t)
+    : undefined);
 
 export const MessageEditor: React.FC<{
   message: QueryData["messages"][number];
@@ -12,12 +21,56 @@ export const MessageEditor: React.FC<{
     React.SetStateAction<number | undefined>
   >;
 }> = ({ message, index: i, data, setData, setSettingMessageIndex }) => {
+  const previewText = getMessageText(message.data);
   return (
-    <div className="mt-4">
-      <div className="font-semibold text-base flex">
-        Message {i + 1}
-        <div className="ml-auto space-x-1"></div>
-      </div>
+    <details className="group/message mt-4 pb-2" open>
+      <summary className="group-open/message:mb-2 transition-[margin] marker:content-none marker-none flex font-semibold text-base cursor-default select-none">
+        <CoolIcon
+          icon="Chevron_Right"
+          className="group-open/message:rotate-90 mr-2 my-auto transition-transform"
+        />
+        <span className="shrink-0">Message {i + 1}</span>
+        {previewText && <span className="truncate ml-1">- {previewText}</span>}
+        <div className="ml-auto space-x-2 my-auto shrink-0">
+          <button
+            className={i === 0 ? "hidden" : ""}
+            onClick={() => {
+              data.messages.splice(i, 1);
+              data.messages.splice(i - 1, 0, message);
+              setData({ ...data });
+            }}
+          >
+            <CoolIcon icon="Chevron_Up" />
+          </button>
+          <button
+            className={i === (data.messages.length - 1) ? "hidden" : ""}
+            onClick={() => {
+              data.messages.splice(i, 1);
+              data.messages.splice(i + 1, 0, message);
+              setData({ ...data });
+            }}
+          >
+            <CoolIcon icon="Chevron_Down" />
+          </button>
+          <button
+            className={data.messages.length >= 10 ? "hidden" : ""}
+            onClick={() => {
+              data.messages.splice(i + 1, 0, message);
+              setData({ ...data });
+            }}
+          >
+            <CoolIcon icon="Copy" />
+          </button>
+          <button
+            onClick={() => {
+              data.messages.splice(i, 1);
+              setData({ ...data });
+            }}
+          >
+            <CoolIcon icon="Trash_Full" />
+          </button>
+        </div>
+      </summary>
       <div className="rounded bg-gray-100 p-2 mt-1 space-y-2">
         <TextArea
           label="Content"
@@ -62,6 +115,6 @@ export const MessageEditor: React.FC<{
           </Button>
         </div>
       </div>
-    </div>
+    </details>
   );
 };
