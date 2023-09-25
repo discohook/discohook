1d52f8d6beacab868c7e60bf68a7391d5a734678
shay <43248357+shayypy@users.noreply.github.com>
2023-09-24 22:21:12 -0500

    feat: backup types, model

---
+++ app/types/discohook.ts
@@ -0,0 +1,66 @@
+import { QueryData } from "./QueryData";
+
+export interface DiscohookBackup {
+  id?: number;
+  name: string;
+  messages: QueryData["messages"];
+  targets: QueryData["targets"];
+}
+
+// https://github.com/discohook/site/blob/main/modules/database/backup/types/ExportData.ts
+
+type MessageData = QueryData["messages"][number]["data"];
+
+export type DiscohookBackupExportData =
+  | {
+      version: 1;
+      name: string;
+      // camel case version of MessageData
+      message: Record<string, unknown>;
+    }
+  | {
+      version: 2;
+      name: string;
+      message: MessageData;
+    }
+  | {
+      version: 3;
+      backups: {
+        name: string;
+        webhookUrl?: string;
+        message: MessageData;
+      }[];
+    }
+  | {
+      version: 4;
+      backups: {
+        name: string;
+        webhookUrl?: string;
+        messages: MessageData[];
+      }[];
+    }
+  | {
+      version: 5;
+      backups: {
+        name: string;
+        messages: MessageData[];
+        target: {
+          url?: string;
+          message?: string;
+        };
+      }[];
+    }
+  | {
+      version: 6;
+      backups: {
+        name: string;
+        messages: QueryData["messages"];
+        target: {
+          url?: string;
+        };
+      }[];
+    }
+  | {
+      version: 7;
+      backups: Omit<DiscohookBackup, "id">[];
+    };
