a7940aab521fd11f8daf53a9fc75fa2727d024c4
shay <43248357+shayypy@users.noreply.github.com>
2023-09-28 09:21:27 -0500

    chore: regenerate entry files

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,9 +1,3 @@
-/**
- * By default, Remix will handle generating the HTTP Response for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.server
- */
-
 import { PassThrough } from "node:stream";
 
 import type { AppLoadContext, EntryContext } from "@remix-run/node";
