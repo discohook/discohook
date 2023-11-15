a768ffbf5360392073400fd04d676cb97aa947c2
shay <43248357+shayypy@users.noreply.github.com>
2023-11-15 16:14:45 -0600

    feat: initial commit
    cloudflare-sample-app + typescript + discord-verify

---
+++ src/server.ts
@@ -0,0 +1,107 @@
+import { Router } from 'itty-router';
+import { PlatformAlgorithm, isValidRequest } from 'discord-verify';
+import { AWW_COMMAND, INVITE_COMMAND } from './commands.js';
+import { getCuteUrl } from './reddit.js';
+import { InteractionType, InteractionResponseType, APIInteractionResponse, MessageFlags } from 'discord-api-types/v10'
+
+export type DiscordInteractionResponse = APIInteractionResponse | { error: string };
+
+class JsonResponse extends Response {
+  constructor(body: DiscordInteractionResponse, init?: ResponseInit | null) {
+    const jsonBody = JSON.stringify(body);
+    init = init || {
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    };
+    super(jsonBody, init);
+  }
+}
+
+const router = Router();
+
+/**
+ * A simple :wave: hello page to verify the worker is working.
+ */
+router.get('/', (request, env) => {
+  return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
+});
+
+/**
+ * Main route for all requests sent from Discord.  All incoming messages will
+ * include a JSON payload described here:
+ * https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object
+ */
+router.post('/', async (request, env) => {
+  const { isValid, interaction } = await server.verifyDiscordRequest(
+    request,
+    env,
+  );
+  if (!isValid || !interaction) {
+    return new Response('Bad request signature.', { status: 401 });
+  }
+
+  if (interaction.type === InteractionType.Ping) {
+    // The `PING` message is used during the initial webhook handshake, and is
+    // required to configure the webhook in the developer portal.
+    return new JsonResponse({
+      type: InteractionResponseType.Pong,
+    });
+  }
+
+  if (interaction.type === InteractionType.ApplicationCommand) {
+    // Most user commands will come as `APPLICATION_COMMAND`.
+    switch (interaction.data.name.toLowerCase()) {
+      case AWW_COMMAND.name.toLowerCase(): {
+        const cuteUrl = await getCuteUrl();
+        return new JsonResponse({
+          type: InteractionResponseType.ChannelMessageWithSource,
+          data: {
+            content: cuteUrl,
+          },
+        });
+      }
+      case INVITE_COMMAND.name.toLowerCase(): {
+        const applicationId = env.DISCORD_APPLICATION_ID;
+        const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${applicationId}&scope=applications.commands`;
+        return new JsonResponse({
+          type: InteractionResponseType.ChannelMessageWithSource,
+          data: {
+            content: INVITE_URL,
+            flags: MessageFlags.Ephemeral,
+          },
+        });
+      }
+      default:
+        return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
+    }
+  }
+
+  console.error('Unknown Type');
+  return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
+});
+router.all('*', () => new Response('Not Found.', { status: 404 }));
+
+async function verifyDiscordRequest(request: Request, env: any) {
+  const signature = request.headers.get('x-signature-ed25519');
+  const timestamp = request.headers.get('x-signature-timestamp');
+  const valid =
+  signature &&
+  timestamp &&
+  (await isValidRequest(request, env.DISCORD_PUBLIC_KEY, PlatformAlgorithm.Cloudflare));
+  if (!valid) {
+    return { isValid: false };
+  }
+
+  const body = await request.text();
+  return { interaction: JSON.parse(body), isValid: true };
+}
+
+const server = {
+  verifyDiscordRequest: verifyDiscordRequest,
+  fetch: async function (request: Request, env: any) {
+    return router.handle(request, env);
+  },
+};
+
+export default server;
