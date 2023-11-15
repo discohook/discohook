5bb3eb472b705b71f90193f386ed2506600cab77
shay <43248357+shayypy@users.noreply.github.com>
2023-11-15 16:26:32 -0600

    chore: rearrange, type interaction

--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,4 @@
-/**
- * Share command metadata from a common spot to be used for both runtime
- * and registration.
- */
+import { APIInteractionResponse } from "discord-api-types/v10";
 
 export const AWW_COMMAND = {
   name: 'awwww',
@@ -12,3 +9,21 @@ export const INVITE_COMMAND = {
   name: 'invite',
   description: 'Get an invite link to add the bot to your server',
 };
+
+export type DiscordInteractionResponse = APIInteractionResponse | { error: string; status?: number };
+
+class JsonResponse extends Response {
+  constructor(body: DiscordInteractionResponse, init?: ResponseInit | null) {
+    const jsonBody = JSON.stringify(body);
+    init = init || {
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    };
+    super(jsonBody, init);
+  }
+}
+
+export const respond = (body: DiscordInteractionResponse) => {
+  return new JsonResponse(body, 'error' in body ? { status: body.status ?? 400 } : null);
+}
