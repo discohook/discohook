a7404456d75f0289a31a908f47966531fdb9e8ab
shay <43248357+shayypy@users.noreply.github.com>
2023-11-26 10:33:32 -0600

    feat: read attachments

--- src/util/cdn.ts
+++ src/util/cdn.ts
@@ -1,5 +1,6 @@
 import { CDN, ImageURLOptions } from "@discordjs/rest";
 import { getUserDefaultAvatar } from "./user.js";
+import { DiscordApiClient } from "discord-api-methods";
 
 export const cdn = new CDN();
 
@@ -17,4 +18,23 @@ export const webhookAvatarUrl = (webhook: { id: string; avatar: string | null },
   } else {
     return cdn.defaultAvatar(5)
   }
+}
+
+export const readAttachment = async (url: string) => {
+  const response = await fetch(url, { method: "GET" });
+  if (!response.ok) {
+    throw new Error(`Failed to fetch attachment with URL ${url}`)
+  }
+  const type = response.headers.get("Content-Type");
+  const buffer = await response.arrayBuffer();
+
+  // This was often failing with files that were 'too large' (a few hundred KB)
+  // so we have to fragmentedly compile the data instead of doing it all at once
+  // Thanks https://stackoverflow.com/a/60782610
+  let binary = '';
+  const bytes = new Uint8Array(buffer);
+  for (let i = 0; i < bytes.byteLength; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return `data:${type ?? "application/octet-stream"};base64,${btoa(binary)}`;
 }
