151161e70d671a3f01a815e2259cfb343d8ceb55
shay <43248357+shayypy@users.noreply.github.com>
2023-11-26 10:36:06 -0600

    chore: rearrange component command callbacks

--- src/commands/components/command.ts
+++ src/commands/components/command.ts
@@ -1,10 +1,10 @@
-import { APIInteraction, APIMessage, Routes } from "discord-api-types/v10";
+import { APIInteraction, APIMessage, MessageFlags, Routes } from "discord-api-types/v10";
 import { ChatInputAppCommandCallback, MessageAppCommandCallback } from "../../commands.js";
 import { InteractionContext } from "../../interactions.js";
 
 const MESSAGE_LINK_RE = /^https:\/\/(?:www\.|ptb\.|canary\.)?discord(?:app)?\.com\/channels\/(\d+)\/(\d+)\/(\d+)$/
 
-export const addComponent = async (ctx: InteractionContext<APIInteraction>, message: APIMessage) => {
+const addComponentCallback = async (ctx: InteractionContext<APIInteraction>, message: APIMessage) => {
   return ctx.reply(message.channel_id);
 }
 
@@ -12,14 +12,17 @@ export const addComponentChatEntry: ChatInputAppCommandCallback = async (ctx) =>
   const messageLink = ctx.getStringOption("message-link").value;
   const match = messageLink.match(MESSAGE_LINK_RE);
   if (!match) {
-    return ctx.reply("Invalid message link. Right click or long-press a message, then use \"Copy Message Link\"");
+    return ctx.reply({
+      content: "Invalid message link. Right click or long-press a message, then use \"Copy Message Link\"",
+      flags: MessageFlags.Ephemeral,
+    });
   }
 
   const message = await ctx.client.get(Routes.channelMessage(match[2], match[3])) as APIMessage;
-  return await addComponent(ctx, message);
+  return await addComponentCallback(ctx, message);
 }
 
 export const addComponentMessageEntry: MessageAppCommandCallback = async (ctx) => {
   const message = ctx.getMessage();
-  return await addComponent(ctx, message);
+  return await addComponentCallback(ctx, message);
 }
