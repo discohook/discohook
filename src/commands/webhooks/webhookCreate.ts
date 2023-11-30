0b59c9df0827984a9d15fe357c449dd0235a493e
shay <43248357+shayypy@users.noreply.github.com>
2023-11-29 22:33:34 -0600

    feat: store self-created webhooks, check deletion

--- src/commands/webhooks/webhookCreate.ts
+++ src/commands/webhooks/webhookCreate.ts
@@ -1,7 +1,14 @@
-import { APIWebhook, ChannelType, MessageFlags, Routes } from "discord-api-types/v10";
+import { APIGuild, APIWebhook, ChannelType, MessageFlags, RESTJSONErrorCodes, Routes } from "discord-api-types/v10";
 import { ChatInputAppCommandCallback } from "../../commands.js";
 import { readAttachment } from "../../util/cdn.js";
 import { getWebhookUrlEmbed } from "./webhookInfo.js";
+import { getDb, upsertGuild } from "../../db.js";
+import { webhooks } from "../../db-schema.js";
+import { isDiscordError } from "../../util/error.js";
+import dedent from "dedent-js";
+import { color } from "../../util/meta.js";
+import { sleep } from "../../util/sleep.js";
+import { eq } from "drizzle-orm";
 
 export const webhookCreateEntry: ChatInputAppCommandCallback = async (ctx) => {
   const name = ctx.getStringOption('name').value,
@@ -108,8 +115,53 @@ export const webhookCreateEntry: ChatInputAppCommandCallback = async (ctx) => {
     channelType,
   );
 
-  return ctx.reply({
-    embeds: [embed.toJSON()],
-    flags: MessageFlags.Ephemeral,
-  });
+  const db = getDb(ctx.db);
+
+  // Use KV as a timed cache instead of fetching anew every time
+  const guild = await ctx.client.get(Routes.guild(ctx.interaction.guild_id!)) as APIGuild;
+  await upsertGuild(db, guild);
+  await db
+    .insert(webhooks)
+    .values({
+      platform: "discord",
+      id: webhook.id,
+      token: webhook.token,
+      name: webhook.name ?? name,
+      discordGuildId: BigInt(webhook.guild_id!),
+      channelId: webhook.channel_id,
+      avatar: webhook.avatar,
+    })
+    .onConflictDoNothing();
+
+  return [
+    ctx.reply({
+      embeds: [embed.toJSON()],
+      flags: MessageFlags.Ephemeral,
+    }),
+    async () => {
+      await sleep(2000);
+      try {
+        await ctx.client.get(Routes.webhook(webhook.id, webhook.token));
+      } catch (e) {
+        if (isDiscordError(e) && e.code === RESTJSONErrorCodes.UnknownWebhook) {
+          await ctx.followup.editOriginalMessage({
+            embeds: [{
+              title: "Webhook was deleted",
+              description: dedent`
+                Your webhook was created successfully, but it seems like a
+                moderation bot may have deleted it automatically. Some bots
+                detect new webhooks as spam activity. Check your server audit
+                log for more details.
+              `,
+              color,
+            }],
+          });
+          await db.delete(webhooks).where(
+            eq(webhooks.id, webhook.id)
+            .append(eq(webhooks.platform, "discord"))
+          );
+        }
+      }
+    },
+  ];
 }
--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,25 @@
+import * as schema from "./db-schema.js";
 import { drizzle } from "drizzle-orm/d1";
 import { Env } from "./types/env.js";
-import * as schema from "./db-schema.js";
+import { APIGuild } from "discord-api-types/v10";
 
 export const getDb = (db: Env['DB']) => drizzle(db, { schema });
+
+type DB = ReturnType<typeof getDb>
+
+export const upsertGuild = async (db: DB, guild: APIGuild) => {
+  await db
+    .insert(schema.discordGuilds)
+    .values({
+      id: BigInt(guild.id),
+      name: guild.name,
+      icon: guild.icon,
+    })
+    .onConflictDoUpdate({
+      target: schema.discordGuilds.id,
+      set: {
+        name: guild.name,
+        icon: guild.icon,
+      },
+    });
+}
