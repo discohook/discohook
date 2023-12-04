b4b9de84ecd9a733bba4cd1b67e2c12e3a0b80e0
shay <43248357+shayypy@users.noreply.github.com>
2023-12-04 17:03:25 -0600

    feat: kvGet utility function

--- src/util/kv.ts
+++ src/util/kv.ts
@@ -8,8 +8,21 @@ export interface PartialKVGuild {
   icon: string | null;
 }
 
+export async function kvGet(kv: Env['KV'], key: string): Promise<string | null>;
+export async function kvGet<T>(kv: Env['KV'], key: string): Promise<T | null>;
+export async function kvGet<T>(kv: Env['KV'], key: string): Promise<T | string | null> {
+  const cached: string | null = await kv.get(key);
+  if (cached === null) return null;
+
+  try {
+    return JSON.parse(cached) as T;
+  } catch {
+    return cached;
+  }
+}
+
 export const getchGuild = async (client: DiscordApiClient, kv: Env['KV'], guildId: string): Promise<PartialKVGuild> => {
-  const cached: string | null = await kv.get(`cache-guild-${guildId}`);
+  const cached = await kvGet<PartialKVGuild>(kv, `cache-guild-${guildId}`);
   if (!cached) {
     const guild = await client.get(Routes.guild(guildId)) as APIGuild;
     const reduced: PartialKVGuild = {
@@ -24,5 +37,5 @@ export const getchGuild = async (client: DiscordApiClient, kv: Env['KV'], guildI
     );
     return reduced;
   }
-  return JSON.parse(cached) as PartialKVGuild;
+  return cached;
 }
