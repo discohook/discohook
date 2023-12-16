0ba9890358be6a1517756a6c1bdfc2d2cc605a01
shay <43248357+shayypy@users.noreply.github.com>
2023-12-16 12:58:12 -0600

    feat: link support server in help modal

--- app/modals/HelpModal.tsx
+++ app/modals/HelpModal.tsx
@@ -1,14 +1,24 @@
+import { ButtonStyle, ComponentType } from "discord-api-types/v10";
 import { Button } from "~/components/Button";
+import { PreviewButton } from "~/components/preview/Components";
 import { Modal, ModalProps } from "./Modal";
 
 export const HelpModal = (props: ModalProps) => {
   return (
     <Modal title="Help" {...props}>
-      <p></p>
+      <p>Searchable box of all the help tags</p>
       <div className="flex w-full mt-4">
-        <Button onClick={() => props.setOpen(false)} className="mx-auto">
-          OK
-        </Button>
+        <div className="flex gap-2 mx-auto">
+          <Button onClick={() => props.setOpen(false)}>OK</Button>
+          <PreviewButton
+            data={{
+              type: ComponentType.Button,
+              style: ButtonStyle.Link,
+              url: "/discord",
+              label: "Support Server",
+            }}
+          />
+        </div>
       </div>
     </Modal>
   );
