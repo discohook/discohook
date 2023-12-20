bb96aa305d58e766ca3f10687f51dbd7a833ebe8
shay <43248357+shayypy@users.noreply.github.com>
2023-12-20 15:23:26 -0600

    update: twemoji to 15.0.3

--- app/components/Twemoji.tsx
+++ app/components/Twemoji.tsx
@@ -1,24 +1,31 @@
+import twemoji from "@twemoji/api";
 import { memo } from "react";
-import twemoji from "twemoji";
 
 // Thanks https://gist.github.com/chibicode/fe195d792270910226c928b69a468206
-const Twemoji_: React.FC<{
-  emoji: string;
-  className?: string;
-  title?: string;
-}> = ({ emoji, className, title }) => (
+const Twemoji_: React.FC<
+  (
+    | { emoji: string; unified?: undefined }
+    | { emoji?: undefined; unified: string }
+  ) & {
+    className?: string;
+    title?: string;
+  }
+> = ({ emoji, unified, className, title }) => (
   <span
     title={title}
     dangerouslySetInnerHTML={{
-      __html: twemoji.parse(emoji, {
-        folder: "svg",
-        ext: ".svg",
-        // https://github.com/twitter/twemoji/issues/580
-        base: "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/",
-        className: `inline-block w-auto h-4 align-[-0.125em] ${
-          className ?? ""
-        }`,
-      }),
+      __html: twemoji.parse(
+        unified
+          ? unified.split("-").map(twemoji.convert.fromCodePoint).join("")
+          : emoji!,
+        {
+          folder: "svg",
+          ext: ".svg",
+          className: `inline-block w-auto h-4 align-[-0.125em] ${
+            className ?? ""
+          }`,
+        }
+      ),
     }}
   />
 );
