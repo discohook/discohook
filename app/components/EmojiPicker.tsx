d6434a1a1670563ab51278d06617e901aad13ac0
shay <43248357+shayypy@users.noreply.github.com>
2023-12-20 22:20:31 -0600

    feat: emoji search, category jumps

--- app/components/EmojiPicker.tsx
+++ app/components/EmojiPicker.tsx
@@ -20,6 +20,7 @@ import { Twemoji } from "./Twemoji";
 type SelectedEmoji = Omit<Emoji, "skins"> & { skin: Skin };
 
 export interface PickerProps {
+  id: string;
   onEmojiClick: (emoji: SelectedEmoji) => void;
 }
 
@@ -34,31 +35,63 @@ const categoryToEmoji: Record<string, string> = {
   flags: "🏴",
 };
 
-export const CategoryIconButton: React.FC<{ id: string }> = ({ id }) => {
-  const emoji = categoryToEmoji[id];
+export const CategoryIconButton: React.FC<{
+  categoryId: string;
+  id: string;
+}> = ({ categoryId, id }) => {
+  const emoji = categoryToEmoji[categoryId];
   return (
-    <button className="block mx-auto">
+    <button
+      className="block mx-auto"
+      onClick={() => {
+        const sectionHeader = document.getElementById(`${id}-${categoryId}`);
+        if (sectionHeader) {
+          sectionHeader.scrollIntoView();
+        }
+      }}
+    >
       <Twemoji emoji={emoji} className="h-5 grayscale" />
     </button>
   );
 };
 
-const EmojiPicker_: React.FC<PickerProps> = ({ onEmojiClick }) => {
+const GridEmoji: React.FC<{
+  emoji: SelectedEmoji;
+  onEmojiClick: (e: SelectedEmoji) => void;
+  setHoverEmoji: (e: SelectedEmoji) => void;
+}> = ({ emoji, onEmojiClick, setHoverEmoji }) => (
+  <button
+    className="rounded p-1 h-11 w-11 hover:bg-white/10 transition"
+    onClick={() => onEmojiClick(emoji)}
+    onMouseOver={() => setHoverEmoji(emoji)}
+  >
+    <Twemoji
+      emoji={emoji.skin.native}
+      className="h-full w-full"
+      title={emoji.id}
+    />
+  </button>
+);
+
+const EmojiPicker_: React.FC<PickerProps> = ({ id, onEmojiClick }) => {
   const [settings, setSettings] = useLocalStorage();
   const [hoverEmoji, setHoverEmoji] = useState<SelectedEmoji>();
+  const [query, setQuery] = useState("");
 
   const data = emojiData as EmojiMartData;
   const skinTone = settings.skinTone;
 
   return (
-    <div className="rounded bg-gray-300 dark:bg-gray-800 w-96 h-64 border border-black/5 shadow-md flex flex-col">
+    <div className="rounded bg-gray-300 dark:bg-gray-800 w-[385px] h-80 border border-black/5 shadow-md flex flex-col">
       <div className="p-2 shadow border-b border-b-black/5 flex">
         <div className="grow">
           <TextInput
             label=""
             className="w-full"
             placeholder="Find the perfect emoji"
-            disabled
+            onInput={(e) =>
+              setQuery(e.currentTarget.value.toLowerCase().trim())
+            }
           />
         </div>
         <div className="shrink-0 ml-3 mr-1 my-auto">
@@ -88,12 +121,13 @@ const EmojiPicker_: React.FC<PickerProps> = ({ onEmojiClick }) => {
                   : "👏"
               }
               className="h-6 align-[-0.3em] w-6"
+              title="Set skin tone"
             />
           </button>
         </div>
       </div>
       <div className="flex grow h-full overflow-hidden">
-        <div className="w-10 shrink-0 bg-gray-400 dark:bg-gray-900 overflow-y-auto h-full space-y-1 p-1 flex flex-col">
+        <div className="w-10 shrink-0 bg-gray-400 dark:bg-gray-900 overflow-y-auto h-full scrollbar-none space-y-1 p-1 flex flex-col">
           <button className="block mx-auto">
             <CoolIcon icon="Star" className="text-2xl" />
           </button>
@@ -104,53 +138,75 @@ const EmojiPicker_: React.FC<PickerProps> = ({ onEmojiClick }) => {
             {data.categories.map((category) => (
               <CategoryIconButton
                 key={`emoji-category-${category.id}-icon`}
-                id={category.id}
+                categoryId={category.id}
+                id={id}
               />
             ))}
           </div>
         </div>
-        <div className="overflow-y-auto flex flex-col grow">
-          <div className="grow p-1">
-            {data.categories.map((category) => (
-              <div
-                key={`emoji-category-${category.id}-body`}
-                id={category.id}
-                className=""
-              >
-                <p className="uppercase text-sm font-semibold">{category.id}</p>
-                <div className="flex gap-px flex-wrap">
-                  {category.emojis.map((name) => {
-                    const emoji = data.emojis[name];
+        <div className="overflow-y-auto flex flex-col grow select-none">
+          <div className="grow px-1.5 pb-1">
+            {query
+              ? Object.values(data.emojis)
+                  .filter(
+                    (e) =>
+                      e.id.includes(query) ||
+                      e.keywords.map((k) => k.includes(query)).includes(true)
+                  )
+                  .map((emoji) => {
                     const skin =
                       emoji.skins[skinTone === undefined ? 0 : skinTone + 1] ??
                       emoji.skins[0];
                     const selected: SelectedEmoji = { ...emoji, skin };
 
                     return (
-                      <button
-                        key={`emoji-category-${category.id}-emoji-${name}`}
-                        className="rounded p-1 h-11 w-11 hover:bg-white/10 transition"
-                        onClick={() => onEmojiClick(selected)}
-                        onMouseOver={() => setHoverEmoji(selected)}
-                      >
-                        <Twemoji
-                          emoji={skin.native}
-                          className="h-full w-full"
-                          title={emoji.name}
-                        />
-                      </button>
+                      <GridEmoji
+                        key={`emoji-search-${emoji.id}`}
+                        emoji={selected}
+                        onEmojiClick={onEmojiClick}
+                        setHoverEmoji={setHoverEmoji}
+                      />
                     );
-                  })}
-                </div>
-              </div>
-            ))}
+                  })
+              : data.categories.map((category) => (
+                  <div
+                    key={`emoji-category-${category.id}-body`}
+                    id={`${id}-${category.id}`}
+                    className="pt-1"
+                  >
+                    <p
+                      className="uppercase text-sm font-semibold"
+                    >
+                      {category.id}
+                    </p>
+                    <div className="flex gap-px flex-wrap">
+                      {category.emojis.map((name) => {
+                        const emoji = data.emojis[name];
+                        const skin =
+                          emoji.skins[
+                            skinTone === undefined ? 0 : skinTone + 1
+                          ] ?? emoji.skins[0];
+                        const selected: SelectedEmoji = { ...emoji, skin };
+
+                        return (
+                          <GridEmoji
+                            key={`emoji-category-${category.id}-emoji-${emoji.id}`}
+                            emoji={selected}
+                            onEmojiClick={onEmojiClick}
+                            setHoverEmoji={setHoverEmoji}
+                          />
+                        );
+                      })}
+                    </div>
+                  </div>
+                ))}
           </div>
           {hoverEmoji && (
-            <div className="sticky bottom-0 left-0 w-full bg-gray-400 dark:bg-gray-900 flex items-center px-4 py-1.5">
+            <div className="sticky bottom-0 left-0 w-full bg-gray-400 dark:bg-gray-900 flex items-center px-4 py-2">
               <Twemoji
                 emoji={hoverEmoji.skin.native}
-                className="h-7 my-auto shrink-0"
-                title={hoverEmoji.name}
+                className="h-7 my-auto shrink-0 !align-bottom"
+                title={hoverEmoji.id}
               />
               <p className="ml-2 text-base font-semibold my-auto truncate">
                 :{hoverEmoji.id}:
@@ -170,13 +226,16 @@ export const PopoutEmojiPicker: React.FC<{
   setEmoji: (emoji: APIMessageComponentEmoji | undefined) => void;
   isClearable?: boolean;
 }> = ({ emoji, setEmoji }) => {
-  const id = randomString(42);
+  const id = randomString(10);
   // const close = () => {
   //   const parent = document.querySelector<HTMLDetailsElement>(`#${id}`);
   //   if (parent) parent.open = false;
   // };
   return (
-    <details className="relative group/emoji" id={id}>
+    <details
+      className="relative group/emoji"
+      // id={id}
+    >
       <summary className="flex cursor-pointer marker:hidden marker-none">
         <div className="h-9 w-9 rounded flex bg-gray-300 dark:bg-[#292b2f]">
           <div className="m-auto">
@@ -201,6 +260,7 @@ export const PopoutEmojiPicker: React.FC<{
       </summary>
       <div className="absolute z-20 pb-8">
         <EmojiPicker
+          id={id}
           onEmojiClick={(selectedEmoji) => {
             // close();
             const newEmoji: APIMessageComponentEmoji = {
