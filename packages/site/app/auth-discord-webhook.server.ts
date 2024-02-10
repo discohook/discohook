79588954596c9b19dac2c142236de8111e3a60cf
shay <43248357+shayypy@users.noreply.github.com>
2024-02-09 22:42:17 -0600

    feat(site): store oauth webhooks

--- packages/site/app/auth-discord-webhook.server.ts
+++ packages/site/app/auth-discord-webhook.server.ts
@@ -1,13 +1,22 @@
+import { REST } from "@discordjs/rest";
 import {
   createCookie,
   createWorkersKVSessionStorage,
 } from "@remix-run/cloudflare";
 import {
+  APIUser,
   APIWebhook,
   RESTPostOAuth2AccessTokenWithBotAndGuildsAndWebhookIncomingScopeResult,
 } from "discord-api-types/v10";
 import { Authenticator } from "remix-auth";
 import { DiscordStrategy } from "remix-auth-discord";
+import {
+  getDb,
+  getchGuild,
+  upsertDiscordUser,
+  upsertGuild,
+  webhooks,
+} from "./store.server";
 import { Context } from "./util/loader";
 
 export const getDiscordWebhookAuth = (context: Context) => {
@@ -31,10 +40,62 @@ export const getDiscordWebhookAuth = (context: Context) => {
       callbackURL: `${context.origin}/callback/discord-webhook`,
       scope: ["webhook.incoming"],
     },
-    async ({ extraParams }): Promise<APIWebhook> => {
-      return (
-        extraParams as unknown as RESTPostOAuth2AccessTokenWithBotAndGuildsAndWebhookIncomingScopeResult
-      ).webhook;
+    async ({ profile, extraParams }): Promise<APIWebhook> => {
+      const { webhook } =
+        extraParams as unknown as RESTPostOAuth2AccessTokenWithBotAndGuildsAndWebhookIncomingScopeResult;
+
+      const db = getDb(context.env.DATABASE_URL);
+      let guildId: bigint | undefined = undefined;
+      if (webhook.guild_id) {
+        const rest = new REST().setToken(context.env.DISCORD_BOT_TOKEN);
+        try {
+          const guild = await getchGuild(
+            rest,
+            context.env.KV,
+            webhook.guild_id,
+          );
+          const upserted = await upsertGuild(db, guild);
+          guildId = upserted.id;
+        } catch {
+          guildId = undefined;
+        }
+      }
+      let userId: number | undefined = undefined;
+      if (profile.__json) {
+        try {
+          const upserted = await upsertDiscordUser(
+            db,
+            profile.__json as APIUser,
+          );
+          userId = upserted.id;
+        } catch {
+          userId = undefined;
+        }
+      }
+
+      await db
+        .insert(webhooks)
+        .values({
+          platform: "discord",
+          id: webhook.id,
+          name: webhook.name ?? "",
+          avatar: webhook.avatar,
+          channelId: webhook.channel_id,
+          discordGuildId: guildId,
+          userId,
+        })
+        .onConflictDoUpdate({
+          target: [webhooks.platform, webhooks.id],
+          set: {
+            name: webhook.name ?? undefined,
+            avatar: webhook.avatar,
+            channelId: webhook.channel_id,
+            discordGuildId: guildId,
+            userId,
+          },
+        });
+
+      return webhook;
     },
   );
 
