7a1359d2df59350d8f88f13ccb48a84ef4829541
shay <43248357+shayypy@users.noreply.github.com>
2024-02-09 22:44:43 -0600

    chore(site): use entry webhook from its upsert

--- packages/site/app/routes/api.audit-log.ts
+++ packages/site/app/routes/api.audit-log.ts
@@ -1,11 +1,17 @@
+import { REST } from "@discordjs/rest";
 import { json } from "@remix-run/cloudflare";
-import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 import { zx } from "zodix";
 import { getUser } from "~/session.server";
 import { getWebhook, getWebhookMessage } from "~/util/discord";
 import { ActionArgs } from "~/util/loader";
-import { getDb, messageLogEntries, webhooks } from "../store.server";
+import {
+  getDb,
+  getchGuild,
+  messageLogEntries,
+  upsertGuild,
+  webhooks
+} from "../store.server";
 
 export const action = async ({ request, context }: ActionArgs) => {
   const { type, webhookId, webhookToken, messageId, threadId } =
@@ -41,35 +47,42 @@ export const action = async ({ request, context }: ActionArgs) => {
 
   const db = getDb(context.env.DATABASE_URL);
 
-  await db
-    .insert(webhooks)
-    .values({
-      platform: "discord",
-      id: webhookId,
-      name: webhook.name ?? "",
-      avatar: webhook.avatar,
-      channelId: webhook.channel_id,
-      // discordGuild: webhook.guild_id
-      //   ? {
-      //     connectOrCreate: {
-      //       where: { id: BigInt(webhook.guild_id) },
-      //       create: {
-      //         id: BigInt(webhook.guild_id),
-      //         name: "Unknown Server",
-      //       },
-      //     },
-      //   }
-      //   : undefined,
-    })
-    .onConflictDoUpdate({
-      target: [webhooks.platform, webhooks.id],
-      set: {
-        name: webhook.name ?? undefined,
+  let guildId: bigint | undefined = undefined;
+  if (webhook.guild_id) {
+    const rest = new REST().setToken(context.env.DISCORD_BOT_TOKEN);
+    try {
+      const guild = await getchGuild(rest, context.env.KV, webhook.guild_id);
+      const upserted = await upsertGuild(db, guild);
+      guildId = upserted.id;
+    } catch {
+      guildId = undefined;
+    }
+  }
+  const entryWebhook = (
+    await db
+      .insert(webhooks)
+      .values({
+        platform: "discord",
+        id: webhookId,
+        name: webhook.name ?? "",
         avatar: webhook.avatar,
         channelId: webhook.channel_id,
-        // discordGuildId: webhook.guild_id ? makeSnowflake(webhook.guild_id) : null,
-      },
-    });
+        discordGuildId: guildId,
+      })
+      .onConflictDoUpdate({
+        target: [webhooks.platform, webhooks.id],
+        set: {
+          name: webhook.name ?? undefined,
+          avatar: webhook.avatar,
+          channelId: webhook.channel_id,
+          discordGuildId: guildId,
+        },
+      })
+      .returning({
+        id: webhooks.id,
+        discordGuildId: webhooks.discordGuildId,
+      })
+  )[0];
 
   const entry = (
     await db
@@ -96,17 +109,6 @@ export const action = async ({ request, context }: ActionArgs) => {
         messageId: messageLogEntries.messageId,
       })
   )[0];
-
-  const entryWebhook = await db.query.webhooks.findFirst({
-    where: and(
-      eq(webhooks.platform, "discord"),
-      eq(webhooks.id, entry.webhookId),
-    ),
-    columns: {
-      id: true,
-      discordGuildId: true,
-    },
-  });
 
   return { ...entry, webhook: entryWebhook };
 };
