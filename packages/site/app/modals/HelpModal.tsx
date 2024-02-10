cad795db38d6f85a812c1d9ef3450496558d5b3e
shay <43248357+shayypy@users.noreply.github.com>
2024-02-10 09:56:29 -0600

    feat(site): help tags data

--- packages/site/app/modals/HelpModal.tsx
+++ packages/site/app/modals/HelpModal.tsx
@@ -1,12 +1,42 @@
-import { ButtonStyle, ComponentType } from "discord-api-types/v10";
+import { APIEmbed, ButtonStyle, ComponentType } from "discord-api-types/v10";
+import { useState } from "react";
 import { Button } from "~/components/Button";
+import { TextInput } from "~/components/TextInput";
 import { PreviewButton } from "~/components/preview/Components";
+import { Embed } from "~/components/preview/Embed";
+import tags_ from "../../public/help.json";
 import { Modal, ModalProps } from "./Modal";
 
 export const HelpModal = (props: ModalProps) => {
+  const [query, setQuery] = useState("");
+  const tags: Record<string, string | APIEmbed> = tags_;
+
   return (
     <Modal title="Help" {...props}>
-      <p>Searchable box of all the help tags</p>
+      <TextInput
+        label="Search"
+        value={query}
+        onChange={(e) => setQuery(e.currentTarget.value)}
+        className="w-full mb-2"
+        placeholder="How do I ..."
+      />
+      <div className="overflow-y-auto max-h-80 space-y-1">
+        {Object.entries(tags)
+          .filter(
+            ([key, value]) =>
+              key === query ||
+              (typeof value !== "string" &&
+                value.title?.toLowerCase()?.includes(query.toLowerCase())),
+          )
+          .map(([key, value]) => {
+            if (typeof value === "string" && key !== query) return <></>;
+            const data =
+              typeof value === "string" ? (tags[value] as APIEmbed) : value;
+            data.color = data.color ?? 0xff81ff;
+
+            return <Embed embed={data} />;
+          })}
+      </div>
       <div className="flex w-full mt-4">
         <div className="flex gap-2 mx-auto">
           <Button onClick={() => props.setOpen(false)}>OK</Button>
