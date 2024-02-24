25d7af0257ef7cef612c7b7a4eb384ef1427cb60
shay <43248357+shayypy@users.noreply.github.com>
2024-02-23 22:46:31 -0600

    feat(site): link backup code generation retries

--- packages/site/app/routes/api.link-backups.ts
+++ packages/site/app/routes/api.link-backups.ts
@@ -1,3 +1,5 @@
+import { json } from "@remix-run/cloudflare";
+import { eq } from "drizzle-orm";
 import { z } from "zod";
 import { zx } from "zodix";
 import { getUser } from "~/session.server";
@@ -15,12 +17,42 @@ export const action = async ({ request, context }: ActionArgs) => {
   });
 
   const db = getDb(context.env.DATABASE_URL);
+
+  // Roughly 99.7m combinations of 62 characters at a length of 6
+  let length = 6;
+  let code = randomString(length);
+  let tries = 0;
+  while (true) {
+    if (tries >= 10) {
+      throw json({
+        message:
+          "Failed to generate a unique code for this backup. Try again later.",
+      });
+    }
+    const extant = await db.query.linkBackups.findFirst({
+      where: eq(linkBackups.code, code),
+      columns: {
+        id: true,
+      },
+    });
+    if (extant) {
+      tries += 1;
+      if (tries >= 3) {
+        // After 3 tries, increment length by 1 each try
+        length += 1;
+      }
+      code = randomString(length);
+    } else {
+      break;
+    }
+  }
+
   return (
     await db
       .insert(linkBackups)
       .values({
         name,
-        code: randomString(12),
+        code,
         data,
         dataVersion: data.version ?? 1,
         ownerId: user.id,
