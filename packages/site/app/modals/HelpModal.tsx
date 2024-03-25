61893f309a3e5d0adfdc188cabc615789daf6c57
shay <43248357+shayypy@users.noreply.github.com>
2024-03-24 22:12:25 -0500

    chore(site): help padding, colors

--- packages/site/app/modals/HelpModal.tsx
+++ packages/site/app/modals/HelpModal.tsx
@@ -20,22 +20,24 @@ export const HelpModal = (props: ModalProps) => {
         className="w-full mb-2"
         placeholder="How do I ..."
       />
-      <div className="overflow-y-auto max-h-80 space-y-1">
-        {Object.entries(tags)
-          .filter(
-            ([key, value]) =>
-              key === query ||
-              (typeof value !== "string" &&
-                value.title?.toLowerCase()?.includes(query.toLowerCase())),
-          )
-          .map(([key, value]) => {
-            if (typeof value === "string" && key !== query) return <></>;
-            const data =
-              typeof value === "string" ? (tags[value] as APIEmbed) : value;
-            data.color = data.color ?? 0xff81ff;
+      <div className="overflow-y-auto max-h-96 flex flex-col">
+        <div className="mx-auto space-y-4">
+          {Object.entries(tags)
+            .filter(
+              ([key, value]) =>
+                key === query ||
+                (typeof value !== "string" &&
+                  value.title?.toLowerCase()?.includes(query.toLowerCase())),
+            )
+            .map(([key, value]) => {
+              if (typeof value === "string" && key !== query) return <></>;
+              const data =
+                typeof value === "string" ? (tags[value] as APIEmbed) : value;
+              data.color = data.color ?? 0x58b9ff;
 
-            return <Embed embed={data} />;
-          })}
+              return <Embed embed={data} />;
+            })}
+        </div>
       </div>
       <div className="flex w-full mt-4">
         <div className="flex gap-2 mx-auto">
