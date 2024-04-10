ec45610d9fd2c0626a71ee75e31ec7caa8472147
shay <43248357+shayypy@users.noreply.github.com>
2024-04-10 15:45:24 -0500

    feat(site): `guild_id` for webhook auth

--- packages/site/app/routes/auth.discord-webhook.ts
+++ packages/site/app/routes/auth.discord-webhook.ts
@@ -1,11 +1,29 @@
+import { z } from "zod";
 import { getDiscordWebhookAuth } from "~/auth-discord-webhook.server";
 import { LoaderArgs } from "~/util/loader";
+import { zxParseQuery } from "~/util/zod";
 
 export const loader = async ({ request, context }: LoaderArgs) => {
+  const { guildId } = zxParseQuery(request, {
+    guildId: z.ostring(),
+  });
+
   const discordWebhookAuth = getDiscordWebhookAuth(context);
-  const webhook = await discordWebhookAuth.authenticate("discord", request);
-  if (webhook) {
-    discordWebhookAuth.logout(request, { redirectTo: "/auth/discord-webhook" });
+  request.headers.delete("Cookie");
+  try {
+    const webhook = await discordWebhookAuth.authenticate("discord", request);
+    if (webhook) {
+      discordWebhookAuth.logout(request, { redirectTo: request.url });
+    }
+  } catch (e) {
+    const response = e as Response;
+    const loc = response.headers.get("Location");
+    if (guildId && loc) {
+      const url = new URL(loc);
+      url.searchParams.set("guild_id", guildId);
+      response.headers.set("Location", url.href);
+    }
+    return response;
   }
   return null;
 };
