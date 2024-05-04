1cdc7c0ad60b8c05eb09f97bf44b43524a44c815
shay <43248357+shayypy@users.noreply.github.com>
2024-05-03 22:22:01 -0500

    chore(site): font-code

--- packages/site/tailwind.config.ts
+++ packages/site/tailwind.config.ts
@@ -5,6 +5,23 @@ export default {
   theme: {
     fontFamily: {
       sans: ["Whitney", "Helvetica", "sans-serif"],
+      code: [
+        // "gg mono",
+        "Source Code Pro",
+        "Consolas",
+        "Andale Mono WT",
+        "Andale Mono",
+        "Lucida Console",
+        "Lucida Sans Typewriter",
+        "DejaVu Sans Mono",
+        "Bitstream Vera Sans Mono",
+        "Liberation Mono",
+        "Nimbus Mono L",
+        "Monaco",
+        "Courier New",
+        "Courier",
+        "monospace",
+      ],
     },
     extend: {
       colors: {
