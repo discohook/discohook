b54d721444c0078ab03df19589cc65eb47cc0f33
shay <43248357+shayypy@users.noreply.github.com>
2024-06-19 12:22:32 -0500

    feat(site): larger number input increments

--- packages/site/app/components/NumberInput.tsx
+++ packages/site/app/components/NumberInput.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { twJoin } from "tailwind-merge";
-import { CoolIcon } from "./icons/CoolIcon";
 
 export interface NumberInputProps {
   min?: number;
@@ -24,16 +23,16 @@ export const NumberInput: React.FC<NumberInputProps> = (props) => {
     }
   }, [initValue, onChange]);
 
-  const subtract = () => {
-    let val = value === undefined ? 0 : value - 1;
+  const subtract = (amount: number) => {
+    let val = value === undefined ? 0 : value - amount;
     if (props.min) {
       val = Math.max(val, props.min);
     }
     if (onChange) onChange(val);
     setValue(val);
   };
-  const add = () => {
-    let val = value === undefined ? 0 : value + 1;
+  const add = (amount: number) => {
+    let val = value === undefined ? 0 : value + amount;
     if (props.max) {
       val = Math.min(val, props.max);
     }
@@ -49,15 +48,22 @@ export const NumberInput: React.FC<NumberInputProps> = (props) => {
       <input {...inputProps} className="peer" value={value} hidden />
       <div
         className={twJoin(
-          "rounded grid grid-cols-3 h-9 bg-gray-300 peer-focus:border-blurple-500 dark:bg-[#292b2f] peer-invalid:border-rose-400 dark:peer-invalid:border-rose-400 peer-disabled:text-gray-500 peer-disabled:cursor-not-allowed transition",
+          "rounded grid grid-cols-5 h-9 bg-gray-300 peer-focus:border-blurple-500 dark:bg-[#292b2f] peer-invalid:border-rose-400 dark:peer-invalid:border-rose-400 peer-disabled:text-gray-500 peer-disabled:cursor-not-allowed transition",
         )}
       >
         <button
           type="button"
           className="border border-gray-200 dark:border-gray-300/20 dark:bg-gray-900 dark:hover:bg-primary-600 transition rounded-l"
-          onClick={subtract}
+          onClick={() => subtract(10)}
         >
-          <CoolIcon icon="Remove_Minus" />
+          - 10
+        </button>
+        <button
+          type="button"
+          className="border border-gray-200 dark:border-gray-300/20 dark:bg-gray-900 dark:hover:bg-primary-600 transition rounded-l"
+          onClick={() => subtract(1)}
+        >
+          -
         </button>
         <div className="border-y border-gray-200 dark:border-gray-300/20 flex">
           <p className="m-auto">{value ?? ""}</p>
@@ -85,9 +91,16 @@ export const NumberInput: React.FC<NumberInputProps> = (props) => {
         <button
           type="button"
           className="border border-gray-200 dark:border-gray-300/20 dark:bg-gray-900 dark:hover:bg-primary-600 transition rounded-r"
-          onClick={add}
+          onClick={() => add(1)}
         >
-          <CoolIcon icon="Add_Plus" />
+          +
+        </button>
+        <button
+          type="button"
+          className="border border-gray-200 dark:border-gray-300/20 dark:bg-gray-900 dark:hover:bg-primary-600 transition rounded-r"
+          onClick={() => add(10)}
+        >
+          + 10
         </button>
       </div>
     </div>
