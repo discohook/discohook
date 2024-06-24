5ff1b0b102dfb19dfca90c6744ffa8cdc705b8ec
shay <43248357+shayypy@users.noreply.github.com>
2024-06-24 13:27:16 -0500

    feat(bot): `null` emoji

--- packages/bot/src/util/meta.ts
+++ packages/bot/src/util/meta.ts
@@ -4,10 +4,18 @@ export const color = 0x58b9ff;
 
 export const deluxeColor = 0xff81ff;
 
-export const boolEmoji = (value: boolean) =>
-  value ? "<:true:834927244500533258>" : "<:false:834927293633527839>";
+export const boolEmoji = (value: boolean | null) =>
+  value === null
+    ? "<:null:1253688417275871302>"
+    : value
+      ? "<:true:834927244500533258>"
+      : "<:false:834927293633527839>";
 
-export const boolPartialEmoji = (value: boolean): APIMessageComponentEmoji =>
-  value
-    ? { name: "true", id: "834927244500533258" }
-    : { name: "false", id: "834927293633527839" };
+export const boolPartialEmoji = (
+  value: boolean | null,
+): APIMessageComponentEmoji =>
+  value === null
+    ? { name: "null", id: "1253688417275871302" }
+    : value
+      ? { name: "true", id: "834927244500533258" }
+      : { name: "false", id: "834927293633527839" };
