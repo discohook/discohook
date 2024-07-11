35189607f4bd68a8a9735c6ab9dc9cd6104faa3c
shay <43248357+shayypy@users.noreply.github.com>
2024-07-11 16:28:31 -0500

    chore(site): subtext regex

--- packages/site/app/components/preview/Markdown.tsx
+++ packages/site/app/components/preview/Markdown.tsx
@@ -217,7 +217,7 @@ const headingRule = defineRule({
 const footingRule = defineRule({
   capture(source, state, parse) {
     if (!/\n$|^$/.test(state.completed)) return;
-    const match = /^ *-#\s+((?!(?:-#)+)[^\n]+?)(?:-#)*\s*(?:\n|$)/.exec(source);
+    const match = /^-# +((?!(-#)+)[^\n]+?) *(?:\n|$)/.exec(source);
     if (!match) return;
     return {
       size: match[0].length,
