4628ac5921a2a2807c77bae16458be63da608d6b
shay <43248357+shayypy@users.noreply.github.com>
2024-08-03 10:59:14 -0500

    fix(site): awful inconsistent component schema

--- packages/site/app/types/components.ts
+++ packages/site/app/types/components.ts
@@ -12,6 +12,7 @@ import {
   APIButtonComponentWithURL,
   APIChannelSelectComponent,
   APIMentionableSelectComponent,
+  APIMessageActionRowComponent,
   APIRoleSelectComponent,
   APIStringSelectComponent,
   APIUserSelectComponent,
@@ -24,20 +25,40 @@ export const ZodPartialEmoji: z.ZodType<APIMessageComponentEmoji> = z.object({
   animated: z.oboolean(),
 });
 
-export const ZodAPIButtonComponentWithCustomId = z.object({
+export const ZodAPIButtonComponentWithCustomIdBase = z.object({
   type: z.literal(ComponentType.Button),
-  style: z.union([
-    z.literal(ButtonStyle.Primary),
-    z.literal(ButtonStyle.Secondary),
-    z.literal(ButtonStyle.Success),
-    z.literal(ButtonStyle.Danger),
-  ]),
   label: z.ostring(),
   emoji: ZodPartialEmoji.optional(),
   custom_id: z.string(),
   disabled: z.oboolean(),
   flow: ZodDraftFlow.optional(),
-}) satisfies z.ZodType<APIButtonComponentWithCustomId>;
+}) satisfies z.ZodType<Omit<APIButtonComponentWithCustomId, "style">>;
+
+export const ZodAPIButtonComponentWithCustomIdPrimary = z
+  .object({ style: z.literal(ButtonStyle.Primary) })
+  .merge(
+    ZodAPIButtonComponentWithCustomIdBase,
+  ) satisfies z.ZodType<APIButtonComponentWithCustomId>;
+
+export const ZodAPIButtonComponentWithCustomIdSecondary = z
+  .object({ style: z.literal(ButtonStyle.Secondary) })
+  .merge(
+    ZodAPIButtonComponentWithCustomIdBase,
+  ) satisfies z.ZodType<APIButtonComponentWithCustomId>;
+
+export const ZodAPIButtonComponentWithCustomIdDanger = z
+  .object({ style: z.literal(ButtonStyle.Danger) })
+  .merge(
+    ZodAPIButtonComponentWithCustomIdBase,
+  ) satisfies z.ZodType<APIButtonComponentWithCustomId>;
+
+export const ZodAPIButtonComponentWithCustomIdSuccess = z
+  .object({
+    style: z.literal(ButtonStyle.Success),
+  })
+  .merge(
+    ZodAPIButtonComponentWithCustomIdBase,
+  ) satisfies z.ZodType<APIButtonComponentWithCustomId>;
 
 export const ZodAPIButtonComponentWithURL = z.object({
   type: z.literal(ComponentType.Button),
@@ -57,125 +78,123 @@ export const ZodAPIButtonComponentWithSkuId = z.object({
   disabled: z.oboolean(),
 }) satisfies z.ZodType<APIButtonComponentWithSkuId>;
 
-export const ZodAPIButtonComponent = z.union([
-  ZodAPIButtonComponentWithCustomId,
+export const ZodAPIButtonComponent = z.discriminatedUnion("style", [
+  ZodAPIButtonComponentWithCustomIdPrimary,
+  ZodAPIButtonComponentWithCustomIdSecondary,
+  ZodAPIButtonComponentWithCustomIdDanger,
+  ZodAPIButtonComponentWithCustomIdSuccess,
   ZodAPIButtonComponentWithURL,
   ZodAPIButtonComponentWithSkuId,
 ]);
 
-export const ZodAPIStringSelectMenuComponent: z.ZodType<APIStringSelectComponent> =
-  z.object({
-    type: z.literal(ComponentType.StringSelect),
-    custom_id: z.string(),
-    options: z.array(
-      z.object({
-        label: z.string(),
-        value: z.string(),
-        description: z.ostring(),
-        emoji: ZodPartialEmoji.optional(),
-        default: z.oboolean(),
-      }),
-    ),
-    placeholder: z.ostring(),
-    min_values: z.onumber(),
-    max_values: z.onumber(),
-    disabled: z.oboolean(),
-    flows: z.record(z.string(), ZodDraftFlow).optional(),
-  });
+export const ZodAPIStringSelectMenuComponent = z.object({
+  type: z.literal(ComponentType.StringSelect),
+  custom_id: z.string(),
+  options: z.array(
+    z.object({
+      label: z.string(),
+      value: z.string(),
+      description: z.ostring(),
+      emoji: ZodPartialEmoji.optional(),
+      default: z.oboolean(),
+    }),
+  ),
+  placeholder: z.ostring(),
+  min_values: z.onumber(),
+  max_values: z.onumber(),
+  disabled: z.oboolean(),
+  flows: z.record(z.string(), ZodDraftFlow).optional(),
+}) satisfies z.ZodType<APIStringSelectComponent>;
 
-export const ZodAPIRoleSelectMenuComponent: z.ZodType<APIRoleSelectComponent> =
-  z.object({
-    type: z.literal(ComponentType.RoleSelect),
-    custom_id: z.string(),
-    default_values: z
-      .object({
-        id: z.string(),
-        type: z.literal(SelectMenuDefaultValueType.Role),
-      })
-      .array()
-      .optional(),
-    placeholder: z.ostring(),
-    min_values: z.onumber(),
-    max_values: z.onumber(),
-    disabled: z.oboolean(),
-    flow: ZodDraftFlow.optional(),
-  });
+export const ZodAPIRoleSelectMenuComponent = z.object({
+  type: z.literal(ComponentType.RoleSelect),
+  custom_id: z.string(),
+  default_values: z
+    .object({
+      id: z.string(),
+      type: z.literal(SelectMenuDefaultValueType.Role),
+    })
+    .array()
+    .optional(),
+  placeholder: z.ostring(),
+  min_values: z.onumber(),
+  max_values: z.onumber(),
+  disabled: z.oboolean(),
+  flow: ZodDraftFlow.optional(),
+}) satisfies z.ZodType<APIRoleSelectComponent>;
 
-export const ZodAPIUserSelectMenuComponent: z.ZodType<APIUserSelectComponent> =
-  z.object({
-    type: z.literal(ComponentType.UserSelect),
-    custom_id: z.string(),
-    default_values: z
-      .object({
-        id: z.string(),
-        type: z.literal(SelectMenuDefaultValueType.User),
-      })
-      .array()
-      .optional(),
-    placeholder: z.ostring(),
-    min_values: z.onumber(),
-    max_values: z.onumber(),
-    disabled: z.oboolean(),
-    flow: ZodDraftFlow.optional(),
-  });
+export const ZodAPIUserSelectMenuComponent = z.object({
+  type: z.literal(ComponentType.UserSelect),
+  custom_id: z.string(),
+  default_values: z
+    .object({
+      id: z.string(),
+      type: z.literal(SelectMenuDefaultValueType.User),
+    })
+    .array()
+    .optional(),
+  placeholder: z.ostring(),
+  min_values: z.onumber(),
+  max_values: z.onumber(),
+  disabled: z.oboolean(),
+  flow: ZodDraftFlow.optional(),
+}) satisfies z.ZodType<APIUserSelectComponent>;
 
-export const ZodAPIMentionableSelectMenuComponent: z.ZodType<APIMentionableSelectComponent> =
-  z.object({
-    type: z.literal(ComponentType.MentionableSelect),
-    custom_id: z.string(),
-    default_values: z
-      .object({
-        id: z.string(),
-        type: z.union([
-          z.literal(SelectMenuDefaultValueType.User),
-          z.literal(SelectMenuDefaultValueType.Role),
-        ]),
-      })
-      .array()
-      .optional(),
-    placeholder: z.ostring(),
-    min_values: z.onumber(),
-    max_values: z.onumber(),
-    disabled: z.oboolean(),
-    flow: ZodDraftFlow.optional(),
-  });
+export const ZodAPIMentionableSelectMenuComponent = z.object({
+  type: z.literal(ComponentType.MentionableSelect),
+  custom_id: z.string(),
+  default_values: z
+    .object({
+      id: z.string(),
+      type: z.union([
+        z.literal(SelectMenuDefaultValueType.User),
+        z.literal(SelectMenuDefaultValueType.Role),
+      ]),
+    })
+    .array()
+    .optional(),
+  placeholder: z.ostring(),
+  min_values: z.onumber(),
+  max_values: z.onumber(),
+  disabled: z.oboolean(),
+  flow: ZodDraftFlow.optional(),
+}) satisfies z.ZodType<APIMentionableSelectComponent>;
 
-export const ZodAPIChannelSelectMenuComponent: z.ZodType<APIChannelSelectComponent> =
-  z.object({
-    type: z.literal(ComponentType.ChannelSelect),
-    custom_id: z.string(),
-    channel_types: z
-      .array(
-        z.union([
-          z.literal(ChannelType.GuildText),
-          z.literal(ChannelType.GuildVoice),
-          z.literal(ChannelType.GuildCategory),
-          z.literal(ChannelType.GuildAnnouncement),
-          z.literal(ChannelType.AnnouncementThread),
-          z.literal(ChannelType.PublicThread),
-          z.literal(ChannelType.PrivateThread),
-          z.literal(ChannelType.GuildStageVoice),
-          z.literal(ChannelType.GuildDirectory),
-          z.literal(ChannelType.GuildForum),
-          z.literal(ChannelType.GuildMedia),
-        ]),
-      )
-      .optional(),
-    default_values: z
-      .object({
-        id: z.string(),
-        type: z.literal(SelectMenuDefaultValueType.Channel),
-      })
-      .array()
-      .optional(),
-    placeholder: z.ostring(),
-    min_values: z.onumber(),
-    max_values: z.onumber(),
-    disabled: z.oboolean(),
-    flow: ZodDraftFlow.optional(),
-  });
+export const ZodAPIChannelSelectMenuComponent = z.object({
+  type: z.literal(ComponentType.ChannelSelect),
+  custom_id: z.string(),
+  channel_types: z
+    .array(
+      z.union([
+        z.literal(ChannelType.GuildText),
+        z.literal(ChannelType.GuildVoice),
+        z.literal(ChannelType.GuildCategory),
+        z.literal(ChannelType.GuildAnnouncement),
+        z.literal(ChannelType.AnnouncementThread),
+        z.literal(ChannelType.PublicThread),
+        z.literal(ChannelType.PrivateThread),
+        z.literal(ChannelType.GuildStageVoice),
+        z.literal(ChannelType.GuildDirectory),
+        z.literal(ChannelType.GuildForum),
+        z.literal(ChannelType.GuildMedia),
+      ]),
+    )
+    .optional(),
+  default_values: z
+    .object({
+      id: z.string(),
+      type: z.literal(SelectMenuDefaultValueType.Channel),
+    })
+    .array()
+    .optional(),
+  placeholder: z.ostring(),
+  min_values: z.onumber(),
+  max_values: z.onumber(),
+  disabled: z.oboolean(),
+  flow: ZodDraftFlow.optional(),
+}) satisfies z.ZodType<APIChannelSelectComponent>;
 
-export const ZodAPISelectMenuComponent = z.union([
+export const ZodAPISelectMenuComponent = z.discriminatedUnion("type", [
   ZodAPIStringSelectMenuComponent,
   ZodAPIRoleSelectMenuComponent,
   ZodAPIUserSelectMenuComponent,
@@ -183,10 +202,42 @@ export const ZodAPISelectMenuComponent = z.union([
   ZodAPIChannelSelectMenuComponent,
 ]);
 
-export const ZodAPIMessageActionRowComponent = z.union([
-  ZodAPIButtonComponent,
-  ZodAPISelectMenuComponent,
-]);
+export const ZodAPIMessageActionRowComponent = z
+  .object({
+    type: z.union([
+      z.literal(ComponentType.Button),
+      z.literal(ComponentType.ChannelSelect),
+      z.literal(ComponentType.MentionableSelect),
+      z.literal(ComponentType.RoleSelect),
+      z.literal(ComponentType.StringSelect),
+      z.literal(ComponentType.UserSelect),
+    ]),
+  })
+  .passthrough()
+  // I'm not the biggest fan of this, but it's the best method I could figure
+  // out here. We're basically doing `discriminatedUnion` ourself since Zod
+  // doesn't support nesting them.
+  .superRefine(async (val, ctx) => {
+    const schema =
+      val.type === ComponentType.Button
+        ? ZodAPIButtonComponent
+        : ZodAPISelectMenuComponent;
+
+    const parsed = await schema.spa(val);
+    if (!parsed.success) {
+      parsed.error.issues.forEach(ctx.addIssue);
+      return z.NEVER;
+    }
+  })
+  .transform(async (val) => {
+    const schema =
+      val.type === ComponentType.Button
+        ? ZodAPIButtonComponent
+        : ZodAPISelectMenuComponent;
+
+    const parsed = await schema.parseAsync(val);
+    return parsed;
+  }) as unknown as z.ZodType<APIMessageActionRowComponent>;
 
 export const ZodAPIActionRowComponent = z.object({
   type: z.literal(ComponentType.ActionRow),
