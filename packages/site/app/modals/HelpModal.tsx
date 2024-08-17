5b803bfe22646a68be0bf9d373535b1c295f4d94
shay <43248357+shayypy@users.noreply.github.com>
2024-08-16 19:22:03 -0500

    feat(site): searchable guide pages in help modal

--- packages/site/app/modals/HelpModal.tsx
+++ packages/site/app/modals/HelpModal.tsx
@@ -1,29 +1,37 @@
 import { APIEmbed, ButtonStyle, ComponentType } from "discord-api-types/v10";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Button } from "~/components/Button";
 import { TextInput } from "~/components/TextInput";
 import { PreviewButton } from "~/components/preview/Components";
 import { Embed } from "~/components/preview/Embed";
+import { GuideFileMeta } from "~/routes/guide.$";
 import tags_ from "../../public/help/en.json";
 import { ExampleModal } from "./ExampleModal";
-import { Modal, ModalProps } from "./Modal";
-import { PreviewDisclaimerModal } from "./PreviewDisclaimerModal";
+import { Modal, ModalFooter, ModalProps, PlainModalHeader } from "./Modal";
 
 export const HelpModal = (props: ModalProps) => {
   const { t } = useTranslation();
   const [query, setQuery] = useState("");
   const tags: Record<string, string | APIEmbed> = tags_;
 
-  const [showDisclaimer, setShowDisclaimer] = useState(false);
   const [exampleOpen, setExampleOpen] = useState(false);
 
+  const [indexData, setIndexData] = useState<Record<string, GuideFileMeta[]>>();
+  useEffect(() => {
+    if (!indexData && props.open) {
+      fetch("/guide-index.json", { method: "GET" }).then((response) => {
+        if (response.ok)
+          response
+            .json()
+            .then((data) => setIndexData(data as typeof indexData));
+      });
+    }
+  }, [indexData, props.open]);
+
   return (
-    <Modal title={t("help")} {...props}>
-      <PreviewDisclaimerModal
-        open={showDisclaimer}
-        setOpen={setShowDisclaimer}
-      />
+    <Modal {...props}>
+      <PlainModalHeader>{t("help")}</PlainModalHeader>
       <ExampleModal open={exampleOpen} setOpen={setExampleOpen} />
       <TextInput
         label={t("search")}
@@ -47,35 +55,61 @@ export const HelpModal = (props: ModalProps) => {
                 typeof value === "string" ? (tags[value] as APIEmbed) : value;
               data.color = data.color ?? 0x58b9ff;
 
-              return <Embed embed={data} />;
+              return <Embed key={`help-tag-${key}`} embed={data} />;
             })}
+          {indexData &&
+            Object.entries(indexData)
+              .flatMap((entries) =>
+                entries[1].map((e) => {
+                  e.path = entries[0];
+                  return e;
+                }),
+              )
+              .filter(
+                (entry) =>
+                  entry.file === query ||
+                  entry.title.toLowerCase().includes(query.toLowerCase()),
+              )
+              .map((entry) => {
+                const data: APIEmbed = {
+                  provider: { name: "Discohook Guides", url: "/guide" },
+                  title: entry.title,
+                  description: entry.description,
+                  thumbnail: entry.thumbnail
+                    ? { url: entry.thumbnail }
+                    : undefined,
+                  url: `/guide/${entry.path ? `${entry.path}/` : ""}${
+                    entry.file
+                  }`,
+                  color: entry.color || 0x58b9ff,
+                };
+
+                return (
+                  <Embed
+                    key={`help-guide-${entry.path}/${entry.file}`}
+                    embed={data}
+                  />
+                );
+              })}
         </div>
       </div>
-      <div className="flex w-full mt-4">
-        <div className="flex gap-2 mx-auto flex-wrap">
-          <Button onClick={() => props.setOpen(false)}>{t("ok")}</Button>
-          <PreviewButton
-            data={{
-              type: ComponentType.Button,
-              style: ButtonStyle.Link,
-              url: "/discord",
-              label: t("supportServer"),
-            }}
-          />
-          <Button
-            discordstyle={ButtonStyle.Secondary}
-            onClick={() => setExampleOpen(true)}
-          >
-            {t("embedExample")}
-          </Button>
-          <Button
-            discordstyle={ButtonStyle.Secondary}
-            onClick={() => setShowDisclaimer(true)}
-          >
-            {t("previewInfo")}
-          </Button>
-        </div>
-      </div>
+      <ModalFooter className="flex gap-2 flex-wrap">
+        <Button onClick={() => props.setOpen(false)}>{t("ok")}</Button>
+        <PreviewButton
+          data={{
+            type: ComponentType.Button,
+            style: ButtonStyle.Link,
+            url: "/discord",
+            label: t("supportServer"),
+          }}
+        />
+        <Button
+          discordstyle={ButtonStyle.Secondary}
+          onClick={() => setExampleOpen(true)}
+        >
+          {t("embedExample")}
+        </Button>
+      </ModalFooter>
     </Modal>
   );
 };
