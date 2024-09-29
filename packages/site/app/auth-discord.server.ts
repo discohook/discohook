31ade43f5e8028c588b2406a0043333d119df5af
shay <43248357+shayypy@users.noreply.github.com>
2024-09-29 11:20:55 -0500

    chore(site): disable guild cleanup on login

--- packages/site/app/auth-discord.server.ts
+++ packages/site/app/auth-discord.server.ts
@@ -4,7 +4,7 @@ import {
   RESTPostOAuth2AccessTokenResult,
   Routes,
 } from "discord-api-types/v10";
-import { and, eq, inArray, notInArray, sql } from "drizzle-orm";
+import { and, eq, notInArray, sql } from "drizzle-orm";
 import { Authenticator } from "remix-auth";
 import { DiscordStrategy } from "remix-auth-discord";
 import { getSessionStorage } from "./session.server";
@@ -68,57 +68,60 @@ export const getDiscordAuth = (
         const guildIds = guilds.map((g) => g.id);
 
         await db.transaction(async (tx) => {
-          const memberships = await tx.query.discordMembers.findMany({
-            where: (discordMembers, { eq }) =>
-              eq(discordMembers.userId, makeSnowflake(j.id)),
-            columns: { guildId: true },
-          });
-          const membershipGuildIds = memberships.map((m) => m.guildId);
-          const memberlessGuilds =
-            membershipGuildIds.length === 0
-              ? []
-              : await tx.query.discordGuilds.findMany({
-                  columns: { id: true },
-                  where: (
-                    discordGuilds,
-                    { and, isNull, inArray, notInArray, notExists, eq, not },
-                  ) =>
-                    and(
-                      // Ignore guilds that the bot is in
-                      isNull(discordGuilds.botJoinedAt),
-                      // Only relevant guilds to this user
-                      inArray(discordGuilds.id, membershipGuildIds),
-                      // Only select guilds that the user isn't in anymore
-                      guildIds.length === 0
-                        ? sql`true`
-                        : notInArray(
-                            discordGuilds.id,
-                            guildIds.map(makeSnowflake),
-                          ),
-                      // Find guilds with no members other than this user
-                      notExists(
-                        tx
-                          .select()
-                          .from(discordMembers)
-                          .where(
-                            and(
-                              eq(discordMembers.guildId, discordGuilds.id),
-                              not(
-                                eq(discordMembers.userId, makeSnowflake(j.id)),
-                              ),
-                            ),
-                          ),
-                      ),
-                    ),
-                });
-          if (memberlessGuilds.length !== 0) {
-            await tx.delete(discordGuilds).where(
-              inArray(
-                discordGuilds.id,
-                memberlessGuilds.map((g) => g.id),
-              ),
-            );
-          }
+          // TODO: Look into this. This may cause issues with unexpected
+          // cascade deletes due to not every member having a `discordMembers`
+          // record
+          // const memberships = await tx.query.discordMembers.findMany({
+          //   where: (discordMembers, { eq }) =>
+          //     eq(discordMembers.userId, makeSnowflake(j.id)),
+          //   columns: { guildId: true },
+          // });
+          // const membershipGuildIds = memberships.map((m) => m.guildId);
+          // const memberlessGuilds =
+          //   membershipGuildIds.length === 0
+          //     ? []
+          //     : await tx.query.discordGuilds.findMany({
+          //         columns: { id: true },
+          //         where: (
+          //           discordGuilds,
+          //           { and, isNull, inArray, notInArray, notExists, eq, not },
+          //         ) =>
+          //           and(
+          //             // Ignore guilds that the bot is in
+          //             isNull(discordGuilds.botJoinedAt),
+          //             // Only relevant guilds to this user
+          //             inArray(discordGuilds.id, membershipGuildIds),
+          //             // Only select guilds that the user isn't in anymore
+          //             guildIds.length === 0
+          //               ? sql`true`
+          //               : notInArray(
+          //                   discordGuilds.id,
+          //                   guildIds.map(makeSnowflake),
+          //                 ),
+          //             // Find guilds with no members other than this user
+          //             notExists(
+          //               tx
+          //                 .select()
+          //                 .from(discordMembers)
+          //                 .where(
+          //                   and(
+          //                     eq(discordMembers.guildId, discordGuilds.id),
+          //                     not(
+          //                       eq(discordMembers.userId, makeSnowflake(j.id)),
+          //                     ),
+          //                   ),
+          //                 ),
+          //             ),
+          //           ),
+          //       });
+          // if (memberlessGuilds.length !== 0) {
+          //   await tx.delete(discordGuilds).where(
+          //     inArray(
+          //       discordGuilds.id,
+          //       memberlessGuilds.map((g) => g.id),
+          //     ),
+          //   );
+          // }
 
           if (guilds.length !== 0) {
             await tx
