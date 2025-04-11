48269490bb2c18ac123671186754d6aad72d72bd
shay <43248357+shayypy@users.noreply.github.com>
2025-04-11 08:41:01 -0500

    fix(site): dark mode radio contrast

--- packages/site/app/components/Radio.tsx
+++ packages/site/app/components/Radio.tsx
@@ -13,7 +13,7 @@ export const Radio = (
   const { label, description } = props;
 
   return (
-    <label className="flex group/radio select-none rounded py-2 px-3 bg-gray-200 dark:bg-gray-700 cursor-pointer">
+    <label className="flex group/radio select-none rounded py-2 px-3 bg-gray-200 dark:bg-gray-800 cursor-pointer">
       <input type="radio" {...props} className="hidden peer" />
       <ConditionalBox className={props.className} />
       <ConditionalBox className={props.className} check />
